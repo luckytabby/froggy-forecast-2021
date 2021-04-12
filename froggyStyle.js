0aa7bdaf32c73fe721f8c6919e8dd90f146cada9
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-04-12 16:14:39 -0400

    Piped current temperature onto homepage
    Used innerHTML to pipe the current temperature in my location onto the FF homepage as a test. It works!

--- froggyStyle.js
+++ froggyStyle.js
@@ -1,5 +1,7 @@
 function displayTemperature(response) {
-    console.log(response.data);
+    let tempToUser = document.querySelector("#currentTemp");
+    tempToUser.innerHTML = response.data.main.feels_like;
+    console.log(response.data.main.feels_like);
 }
 
 let apiKey = "3fdbb0c1f67069bd33e76ea8a1295d83";
