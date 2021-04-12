6ea6386dacd8f969ce715f5e1461581253df1050
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-04-12 16:00:30 -0400

    Linked to JS file and tested API url
    Linked to JS file and console logged my API url to make sure the apiUrl was working correctly.

--- froggyStyle.js
+++ froggyStyle.js
@@ -1,2 +1,5 @@
 let apiKey = "3fdbb0c1f67069bd33e76ea8a1295d83";
-let apiUrl = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+let cityName = "Morgantown";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+console.log(apiUrl);
