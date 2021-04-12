09e36b5943a7fe359879e9ffcc7f630446b8fa90
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-04-12 16:05:10 -0400

    Added displayTemperature function
    Retrieved data from the Open Weather Map API using the displayTemperature function.

--- froggyStyle.js
+++ froggyStyle.js
@@ -1,5 +1,12 @@
+function displayTemperature(response) {
+    console.log(response.data);
+}
+
 let apiKey = "3fdbb0c1f67069bd33e76ea8a1295d83";
 let cityName = "Morgantown";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`;
 
 console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
