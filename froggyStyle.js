d0b3f21513b79fa2c122a1ded5339a01e049fda4
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-04-13 11:02:27 -0400

    Updated date and time
    Used JS to pull local date/time with help from our weather API.

--- froggyStyle.js
+++ froggyStyle.js
@@ -1,3 +1,37 @@
+function formatDate(timestamp) {
+
+    let date = new Date(timestamp);
+
+    let hours = date.getHours();
+    
+    if (hours < 10) {
+        hours = `0${hour}`;
+    };
+
+    let minutes = date.getMinutes();
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    };
+
+
+    let day = date.getDay();
+
+      let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+  let formattedDay = days[(day)]
+
+    return `${formattedDay} ${hours}:${minutes}`;
+}
+
 function displayWeather(response) {
     let locationToUser = document.querySelector("#currentLocation");
     locationToUser.innerHTML = response.data.name;
@@ -15,7 +49,7 @@ function displayWeather(response) {
     windSpeedToUser.innerHTML = Math.round(response.data.wind.speed);
 
     let dateAndTimeToUser = document.querySelector("#dayAndTime");
-    dateAndTimeToUser.innerHTML = "Friday 5:00 pm";
+    dateAndTimeToUser.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "3fdbb0c1f67069bd33e76ea8a1295d83";
