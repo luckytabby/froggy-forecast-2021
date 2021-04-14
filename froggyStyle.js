afa12b933968da82d6d3a220a31a1b6f79411b4b
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-04-14 09:55:55 -0400

    Edited styles and added footer
    Made some final (for now) tweaks to CSS styles and added a footer to point onlookers to my website and repository.

--- froggyStyle.js
+++ froggyStyle.js
@@ -5,7 +5,7 @@ function formatDate(timestamp) {
     let hours = date.getHours();
     
     if (hours < 10) {
-        hours = `0${hour}`;
+        hours = `0${hours}`;
     };
 
     let minutes = date.getMinutes();
